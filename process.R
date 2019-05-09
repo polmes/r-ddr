a5f5c86a449e4ffea4970e336622dfa7b31526fb
Qjammer <baladoramiro+github@gmail.com>
2019-05-09 21:26:18 +0200

    removed cpp stuff

--- process.R
+++ process.R
@@ -1,14 +1,22 @@
 library(data.table)
 library(fasttime)
 tables<-function(file){
-	colClasses<-list(factor=c("PtID_st","PtID_end","ICAO_orig","ICAO_dest","AC_type","Callsign"))
+	colnames<-c("SegID","ICAO_orig","ICAO_dest","AC_type",
+				"time1","time2","FL1","FL2",
+				"Callsign","Date_st","Date_end",
+				"Lat_st","Lon_st","Lat_end","Lon_end","FlID","seq","Dist")
+	#colClasses<-list(factor=c("Time1","Time2","Date1","Date2"))
+	colClasses<-list(character=c(5,6,11,12))
 	message("Reading File")
-	ddr2<-fread(file,stringsAsFactors=FALSE,colClasses=colClasses)
-	
-	if(class(ddr2$Date_st)[1]!="POSIXct"){
-		ddr2[,c("Date_st","Date_end") := list(fastPOSIXct(Date_st),fastPOSIXct(Date_end))]
-	}
-	ddr2[,Dist_cum:=cumsum(Length),by=FlID]
+	ddr2<-fread(file,drop=c(9,19),stringsAsFactors=TRUE,col.names=colnames,colClasses=colClasses)
+	message("Splitting IDs")
+	ddr2[, c("PtID_st", "PtID_end") := tstrsplit(SegID, "_", fixed = TRUE)]
+	message("Converting to dates")
+	ddr2[, Date_st := fastPOSIXct(paste(substr(Date_st,1,2),"-",substr(Date_st,3,4),"-",substr(Date_st,5,6),"T", substr(time1,1,2),":",substr(time1,3,4),":",substr(time1,5,6)))]
+	ddr2[, Date_end := fastPOSIXct(paste(substr(Date_end,1,2),"-",substr(Date_end,3,4),"-",substr(Date_end,5,6),"T", substr(time2,1,2),":",substr(time2,3,4),":",substr(time2,5,6)))]
+	ddr2[,c("time1","time2"):=NULL]
+	message("Cummulative distance")
+	ddr2[,Dist_cum:=cumsum(Dist),by=FlID]
 	
 	
 	# Flights Data (FlID, Callsign, ICAO_orig, ICAO_dest, Total_dist)
@@ -19,19 +27,19 @@ tables<-function(file){
 
 
 	# Points Data (PtID Date_min Date_max Lon Lat)
-	message("Processing Points Data")
-	ptsstart<-ddr2[seq==1,.(PtID_st,Date_st,Lat_st,Lon_st)]
-	ptsrest<-ddr2[,.(PtID_end,Date_end,Lat_end,Lon_end)]
-	PtcolNames<-c("PtID","Date","Lat","Lon")
-	names(ptsstart)<-PtcolNames
-	names(ptsrest)<-PtcolNames
-	points=rbindlist(list(ptsstart,ptsrest))
-	setorder(points,"PtID","Date")
-	points[, `:=` (minDate=min(Date),maxDate=max(Date)),by=c("PtID","Lat","Lon")]
-	points[,Date:=NULL]
-	points<-unique(points)
+	#message("Processing Points Data")
+	#ptsstart<-ddr2[seq==1,.(PtID_st,Date_st,Lat_st,Lon_st)]
+	#ptsrest<-ddr2[,.(PtID_end,Date_end,Lat_end,Lon_end)]
+	#PtcolNames<-c("PtID","Date","Lat","Lon")
+	#names(ptsstart)<-PtcolNames
+	#names(ptsrest)<-PtcolNames
+	#points=rbindlist(list(ptsstart,ptsrest))
+	#setorder(points,"PtID","Lat","Lon")
+	#points[, `:=` (minDate=min(Date),maxDate=max(Date)),by=c("PtID","Lat","Lon")]
+	#points[,Date:=NULL]
+	#points<-unique(points)
 	
-	ddr2[,c("Lat_st","Lon_st","Lat_end","Lon_end"):=NULL]
+	#ddr2[,c("Lat_st","Lon_st","Lat_end","Lon_end"):=NULL]
 
 	# Airplane Data (Callsign Date_min Date_max AC_type)
 	message("Processing Airplane Data")
@@ -45,10 +53,10 @@ tables<-function(file){
 	
 	# Trajectory Data
 	message("Processing Trajectory Data")
-	trajstart<-ddr2[seq==1,.(FlID,PtID_st,Date_st)]
-	trajrest<-ddr2[,.(FlID,PtID_end,Date_end,seq,Length,Dist_cum)];
-	trajstart[,c("seq","Length","Dist_cum"):=list(0,0,0)]
-	TrajColnames<-c("FlID","PtID","Date","seq","Dist_to_prev","Dist_cum")
+	trajstart<-ddr2[seq==1,.(FlID,PtID_st,Date_st,Lat_st,Lon_st)]
+	trajrest<-ddr2[,.(FlID,PtID_end,Date_end,Lat_end,Lon_end,seq,Dist,Dist_cum)]
+	trajstart[,c("seq","Dist","Dist_cum"):=list(0,0,0)]
+	TrajColnames<-c("FlID","PtID","Date","Lat","Lon","seq","Dist_to_prev","Dist_cum")
 	names(trajstart)<-TrajColnames
 	names(trajrest)<-TrajColnames
 	trajectory<-rbindlist(list(trajstart,trajrest))
@@ -58,6 +66,6 @@ tables<-function(file){
 	return(list(trajectory,flights,planes,points))
 }
 system.time(
-tb<-tables("data/20160222_20160226_0000_2359_____m3.so6.pre")
+tb<-tables("data/20160222_20160226_0000_2359_____m3.so6")
 )
 format(object.size(tb),units="MiB")
