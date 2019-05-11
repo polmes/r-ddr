a6104356a7030993733809c2db85ef05af6d5eb3
polmes <pol.developer1@gmail.com>
2019-05-11 13:40:52 +0200

    Load and install package requirements if necessary

--- main.R
+++ main.R
@@ -1,7 +1,17 @@
-library('data.table')
+# Define required packages
+requirements <- c('data.table', 'fasttime', 'ggplot2')
 
-# rm(list=ls())
+# Check requirements
+missing <- requirements[!(requirements %in% installed.packages()[,'Package'])]
+if (length(missing)) {
+	message('Installing missing packages...')
+	install.packages(missing)
+}
+
+# Load requirements
+slibrary <- function(pkg) { suppressPackageStartupMessages(library(pkg, character.only=TRUE, quietly=TRUE)) }
+lapply(requirements, slibrary)
+
+# Set working directory to source directory
 sourceDir <- getSrcDirectory(function() {}) # R is dumb
 setwd(sourceDir)
-
-# getwd()
