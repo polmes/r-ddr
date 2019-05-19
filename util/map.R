16f40f375ccbbf80ac43c4300ffbfbe09b9ed633
polmes <pol.developer1@gmail.com>
2019-05-19 12:29:57 +0200

    Fix code formatting issues

--- util/map.R
+++ util/map.R
@@ -8,19 +8,16 @@ ddr.map <- function(routes, autocenter = TRUE) {
 			axis.line = element_blank(), axis.ticks = element_blank(),
 			axis.text.x = element_blank(), axis.text.y = element_blank(),
 			axis.title.x = element_blank(), axis.title.y = element_blank(),
-			panel.border = element_rect(fill = NA, color = 'black')
-		) +
+			panel.border = element_rect(fill = NA, color = 'black')) +
 		if (autocenter) {
 			coord_map('ortho',
 					  orientation = c(median(routes[, lat], na.rm = TRUE), median(routes[, lon], na.rm = TRUE), 0),
 					  xlim = c(min(routes[, lon], na.rm = TRUE) - 2, max(routes[, lon], na.rm = TRUE) + 2),
-					  ylim = c(min(routes[, lat], na.rm = TRUE) - 2, max(routes[, lat], na.rm = TRUE) + 2)
-			)
+					  ylim = c(min(routes[, lat], na.rm = TRUE) - 2, max(routes[, lat], na.rm = TRUE) + 2))
 		} else {
 			coord_map('ortho',
 					  orientation = c(50.8, 4.4, 0), # Brussels
-					  xlim = c(-20, 50), ylim = c(32, 70) # Europe
-			)
+					  xlim = c(-20, 50), ylim = c(32, 70)) # Europe
 		}
 
 	print(map)
