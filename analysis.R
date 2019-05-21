840a5e339185d73095e11a915d843dbac1124e85
polmes <pol.developer1@gmail.com>
2019-05-21 21:50:21 +0200

    Convert 'event' to Date and find 'UFO'-type flights info

--- analysis.R
+++ analysis.R
@@ -1,31 +1,39 @@
 # Define event
-bru <- 'EBBR'
-coord <- c(50.90139, 4.484444) # lat, lon
-event <- c('2016-03-22', '2016-04-03')
+bru <- 'EBBR' # airport ICAO code
+coord <- c(50.90139, 4.484444) # location (lat, lon)
+event <- c('2016/03/22', '2016/04/03') # interval (closes, reopens)
 
-# Find diverted flights
-div <- merge(data$real$routes[, .SD[.N], by = id, .SDcols = c('pt')],
-			 data$real$flights[, .(id, dest, as.Date(landing))], by = c('id'))
-div <- div[dest == bru & pt != dest & V3 == event[1]] # v3 = date
-div <- div[, c('dest', 'V3') := NULL]
-
-# Fuel burn
-fuelburn <- 6.1264 # fuel burn [kg/NM] average of A320's and B737's for short-haul flights
-info <- merge(data$real$flights[id %in% div[, id], .(id, airline, orig, cumdist)],
-			  data$plan$flights[id %in% div[, id], .(id, cumdist)],
-			  by = c('id'))
-info[, diff := cumdist.x - cumdist.y]
-info[, c('cumdist.x', 'cumdist.y') := NULL]
-info <- merge(div, info, by = c('id'))
-info[, fuel := fuelburn * diff]
+# Convert to dates
+event <- as.Date(event, '%Y/%m/%d')
 
 # Airlines
 airlines <- readRDS(ddr.path('airlines'))
+airlines <- airlines[month == paste0(format(event[1], '%Y-%m'), '-01')]
 # airlines <- airlines[month == unique(airlines$month)[which.min(abs(unique(airlines$month) - as.Date(event[1], '%Y-%m-%d')))]]
-airlines <- airlines[month == paste0(format(as.Date(event[1], '%Y-%m-%d'), '%Y-%m'), '-01')]
-info <- merge(info, airlines[, .(ICAO, name)], by.x = c('airline'), by.y = c('ICAO'))
-info <- info[, airline := NULL]
 
-# Final table
-info <- info[, .(id, orig, pt, name, diff, fuel)]
-setnames(info, c('id', 'orig', 'dest', 'airline', 'diff', 'fuel'))
+# Fuel burn
+fuelburn <- 6.1264 # fuel burn [kg/NM] average of A320's and B737's for short-haul flights
+
+# Diverted flights
+div <- merge(data$real$flights[, .(id, as.Date(landing), orig, dest)],
+			 data$real$routes[, .SD[.N], by = id, .SDcols = c('pt')], by = c('id'))
+div <- div[dest == bru & pt != dest & V2 == event[1]] # V2 = date
+div <- div[, c('dest', 'V2') := NULL]
+div <- merge(div, data$real$flights[id %in% div[, id], .(id, airline, cumdist)], by = c('id'))
+div <- merge(div, data$plan$flights[id %in% div[, id], .(id, cumdist)], by = c('id'))
+div[, diff := cumdist.x - cumdist.y]
+div[, c('cumdist.x', 'cumdist.y') := NULL]
+div[, fuel := fuelburn * diff]
+div <- merge(div, airlines[, .(ICAO, name)], by.x = c('airline'), by.y = c('ICAO'))
+div <- div[, airline := NULL]
+div <- div[, .(id, orig, pt, name, diff, fuel)]
+setnames(div, c('id', 'orig', 'dest', 'airline', 'diff', 'fuel'))
+
+# Special flights while airport was closed ("UFOs")
+ufo <- data$real$flights[(orig == bru | dest == bru) &
+						 (as.Date(takeoff) > event[1] & as.Date(takeoff) < event[2]),
+						 .(id, orig, dest, airline, aircraft, takeoff, landing)]
+ufo <- merge(ufo, airlines[, .(ICAO, name)], by.x = c('airline'), by.y = c('ICAO'))
+ufo[, airline := NULL]
+ufo <- ufo[, .(id, orig, dest, name, aircraft, takeoff, landing)]
+setnames(ufo, c('id', 'orig', 'dest', 'airline', 'aircraft', 'takeoff', 'landing'))
