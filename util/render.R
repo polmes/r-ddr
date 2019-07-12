c9a5cb877b2eacf3f92f1423c212c8b63b56b64d
polmes <pol.developer1@gmail.com>
2019-07-12 16:42:57 +0200

    Add support for multiple formats (i.e., html/pdf) in util.render function

--- util/render.R
+++ util/render.R
@@ -1,6 +1,10 @@
-ddr.render <- function(file, dir = 'report') {
+ddr.render <- function(file, dir = 'report', format = 'html') {
 	rep <- rmarkdown::render(file.path(dir, file), quiet = TRUE)
-	tmp <- tempfile(file, fileext = '.html')
-	file.copy(rep, tmp)
-	rstudioapi::viewer(tmp)
+	if (format == 'html') {
+		tmp <- tempfile(file, fileext = paste0('.', format))
+		file.copy(rep, tmp)
+		rstudioapi::viewer(tmp)
+	} else if (format == 'pdf') {
+		system2(Sys.getenv('R_PDFVIEWER'), paste0(file.path(dir, tools::file_path_sans_ext(file)), '.pdf'))
+	}
 }
