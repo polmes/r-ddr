092f4acc7bddcc37cc03179c0f16e98c1a9e9dde
polmes <pol.developer1@gmail.com>
2019-07-13 18:27:28 +0200

    Added option to control interval date breaks

--- util/stimes.R
+++ util/stimes.R
@@ -1,4 +1,4 @@
-ddr.stimes <- function(flights, airport, event, onlydest = FALSE) {
+ddr.stimes <- function(flights, airport, event, onlydest = FALSE, interval = '1 week') {
 	cols <- c('date', 'N')
 	flightsWithDest <- flights[dest == airport, .N, by = as.Date(landing)]
 	setnames(flightsWithDest, cols)
@@ -15,20 +15,20 @@ ddr.stimes <- function(flights, airport, event, onlydest = FALSE) {
 		ggplot(data = allFlights, aes(x = date)) +
 			geom_line(aes(y = N.x, color = paste(airport, 'Departures'))) +
 			geom_line(aes(y = N.y, color = paste(airport, 'Arrivals'))) +
-			scale_x_date(date_breaks = '1 week', date_labels = '%d/%m') +
+			scale_x_date(date_breaks = interval, date_labels = '%d/%m') +
 			geom_vline(xintercept = event, linetype = 2) +
 			labs(x = NULL, y = 'Number of Flights', color = NULL) +
-			annotate('text', x = event[1], label = 'Airport closes', y = 10, angle = 90, vjust = -1, hjust = 0) +
-			annotate('text', x = event[2], label = 'Airport reopens', y = 10, angle = 90, vjust = -1, hjust = 0) +
+			annotate('text', x = event[1], label = 'Airport closes', y = -Inf, angle = 90, vjust = -1, hjust = -0.1) +
+			annotate('text', x = event[2], label = 'Airport reopens', y = -Inf, angle = 90, vjust = -1, hjust = -0.1) +
 			theme(legend.position = 'top')
 	} else {
 		ggplot(data = allFlights, aes(x = date)) +
 			geom_line(aes(y = N, color = paste(airport, 'Arrivals'))) +
-			scale_x_date(date_breaks = '1 week', date_labels = '%d/%m') +
+			scale_x_date(date_breaks = interval, date_labels = '%d/%m') +
 			geom_vline(xintercept = event, linetype = 2) +
 			labs(x = NULL, y = 'Number of Flights', color = NULL) +
-			annotate('text', x = event[1], label = 'Airport closes', y = 10, angle = 90, vjust = -1, hjust = 0) +
-			annotate('text', x = event[2], label = 'Airport reopens', y = 10, angle = 90, vjust = -1, hjust = 0) +
+			annotate('text', x = event[1], label = 'Airport closes', y = -Inf, angle = 90, vjust = -1, hjust = -0.1) +
+			annotate('text', x = event[2], label = 'Airport reopens', y = -Inf, angle = 90, vjust = -1, hjust = -0.1) +
 			theme(legend.position = 'top')
 	}
 }
